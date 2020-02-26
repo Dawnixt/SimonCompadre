b81810c312b0215a37f301992c6c451a4c7e6488
Dawnixt <danielgordillo9@gmail.com>
2020-02-26 14:39:06 +0100

    gg

--- SimonCompadre/SimonCompadre-UI/ViewModel/clsMainPageVM.cs
+++ SimonCompadre/SimonCompadre-UI/ViewModel/clsMainPageVM.cs
@@ -50,63 +50,32 @@ namespace SimonCompadre_UI.ViewModel
         #endregion
 
         #region Metodos
-        public void generarOrden()
+        public void generarSiguiente()
         {
             Random btnSiguiente = new Random();
-            //for (int i = 0; i < 12; i++)
-            //{
 
-            //    idBoton = btnSiguiente.Next(1, 7);
-
-            //    switch (idCarta)
-            //    {
-            //        case 1:
-            //            if(numero1 < 2)
-            //            {
-            //                cartas.Add(new clsCarta(1, "ms-appx:///Assets/Adorable.jpg"));
-            //                numero1++;
-            //            }
-            //            break;
-            //        case 2:
-            //            if (numero2 < 2)
-            //            {
-            //                cartas.Add(new clsCarta(2, "ms-appx:///Assets/SakuWitch.png"));
-            //                numero2++;
-            //            }
-            //            break;
-            //        case 3:
-            //            if (numero3 < 2)
-            //            {
-            //                cartas.Add(new clsCarta(3, "ms-appx:///Assets/KaminaJimaru.jpg"));
-            //                numero3++;
-            //            }
-            //            break;
-            //        case 4:
-            //            if (numero4 < 2)
-            //            {
-            //                cartas.Add(new clsCarta(4, "ms-appx:///Assets/Mercy.jpg"));
-            //                numero4++;
-            //            }
-            //            break;
-            //        case 5:
-            //            if (numero5 < 2)
-            //            {
-            //                cartas.Add(new clsCarta(5, "ms-appx:///Assets/SOXVA.jpg"));
-            //                numero5++;
-            //            }
-            //            break;
-            //        case 6:
-            //            if (numero6 < 2)
-            //            {
-            //                cartas.Add(new clsCarta(6, "ms-appx:///Assets/ChaldeaPark.jpg"));
-            //                numero6++;
-            //            }
-            //            break;
-            //    }
-
-            //}
+            int idBoton = btnSiguiente.Next(1, 7);
+            //TODO sacar de un video el sonido para jugar
+            switch (idBoton)
+            {
+                case 1:
+                    _orden.Add(new clsBoton("sonidp", "Hola", 1));
+                    break;
+                case 2:
+                    _orden.Add(new clsBoton("sonidp", "Hola", 2));
+                    break;
+                case 3:
+                    _orden.Add(new clsBoton("sonidp", "Hola", 3));
+                    break;
+                case 4:
+                    _orden.Add(new clsBoton("sonidp", "Hola", 4));
+                    break;
+            }
 
         }
+
+
+
         #endregion
 
     }
